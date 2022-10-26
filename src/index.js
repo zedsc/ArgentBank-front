02af6c2f300ea88a5f42204e8ebf64956bd71ed4
ZSchmitt <zaiane.schmitt@gmail.com>
2022-10-26 18:55:46 +0200

    feat: global style

--- src/index.js
+++ src/index.js
@@ -4,12 +4,31 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./app/store";
 import { BrowserRouter } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import { degradedGreys } from "./utils/colors";
+
+const GlobalStyle = createGlobalStyle`
+* {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: ${degradedGreys.gray2};
+}
+
+body {
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
+        <GlobalStyle />
         <App />
       </Provider>
     </BrowserRouter>
