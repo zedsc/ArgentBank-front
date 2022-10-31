b947ef27368c02692964064d6e98c9a8464a9752
ZSchmitt <zaiane.schmitt@gmail.com>
2022-10-31 18:32:37 +0100

    feat: var colors

---
+++ src/utils/colors.js
@@ -0,0 +1,13 @@
+export const degradedGreys = {
+  gray1: "#ccc",
+  gray2: "#2c3e50",
+  gray3: "#12002b",
+  gray4: "#222",
+};
+
+export const colors = {
+  primary: "#00bc77",
+  secondary: "#42b983",
+  white: "#fff",
+  black: "#000",
+};
