160ee4eb015be9164a7879121957d6ceccdedcff
ZSchmitt <zaiane.schmitt@gmail.com>
2022-11-04 22:41:28 +0100

    feat: sign in page

---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import Footer from "../components/Footer";
+import Header from "../components/header";
+import SignInForm from "../components/SignInForm";
+import { degradedGreys } from "../utils/colors";
+
+const DarkBg = styled.main`
+  flex: 1;
+  background-color: ${degradedGreys.gray3};
+`;
+
+const PageContent = styled.div`
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const SignIn = () => {
+  return (
+    <PageContent>
+      <Header />
+      <DarkBg>
+        <SignInForm />
+      </DarkBg>
+      <Footer />
+    </PageContent>
+  );
+};
+
+export default SignIn;
