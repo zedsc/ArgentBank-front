43aa8864d6ab806415d974498179625bd3f1e5b0
ZSchmitt <zaiane.schmitt@gmail.com>
2022-11-11 18:24:03 +0100

    feat: user = auth slices

---
+++ src/feature/auth.slice.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const BASE_URL = "http://localhost:3001/api/v1/user/";
+
+const userToken = sessionStorage.getItem("token")
+  ? sessionStorage.getItem("token")
+  : null;
+
+// Authentification - login
+export const logIn = createAsyncThunk(
+  "auth/login",
+  async (loginCredentials, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(BASE_URL + "login", loginCredentials);
+      console.log(response.data.body.token);
+      return response?.data.body.token;
+    } catch (err) {
+      console.log(err);
+      return rejectWithValue(err.response.data);
+    }
+  }
+);
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    userName: "",
+    userToken,
+    rememberMe: false,
+    status: "idle",
+    error: "false",
+  },
+  reducers: {
+    setRememberMe: (state, { payload }) => {
+      state.rememberMe = payload;
+    },
+    setAuthLogout: (state) => {
+      state.userName = "";
+      state.userToken = null;
+      state.rememberMe = false;
+      state.status = "idle";
+      state.error = "false";
+      sessionStorage.clear();
+      localStorage.clear();
+    },
+  },
+  extraReducers: {
+    [logIn.pending]: (state) => {
+      state.status = "loading";
+    },
+    [logIn.fulfilled]: (state, { payload }) => {
+      state.status = "succeded";
+      state.userToken = payload;
+    },
+    [logIn.rejected]: (state, { payload }) => {
+      state.status = "failed";
+      state.error = payload.message;
+    },
+  },
+});
+
+export const { setRememberMe, setAuthLogout } = authSlice.actions;
+export default authSlice.reducer;
---
+++ src/feature/user.slice.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const BASE_URL = "http://localhost:3001/api/v1/user/";
+
+// Get user datas
+export const getUserInfos = createAsyncThunk(
+  "user/getDatas",
+  async (token, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(BASE_URL + "profile", "", token);
+      console.log(response.data.body);
+      return response?.data.body;
+    } catch (err) {
+      console.log(err);
+      return rejectWithValue(err.response.data);
+    }
+  }
+);
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    userDatas: {},
+    status: "idle",
+    error: "false",
+  },
+  reducers: {
+    setUserDatas: (state, { payload }) => {
+      // { payload } destructuré = action.payload, ancinnement action à la place de
+      //{ payload } qu'on peut remplacer par payload tout seul
+      state.userDatas = payload; //user défini au-dessus
+    },
+    setIsAuth: (state, { payload }) => {
+      state.isAuth = payload;
+    },
+    setUserLogout: (state) => {
+      state.userDatas = {};
+      state.status = "idle";
+      state.rror = "false";
+    },
+  },
+  extraReducers: {
+    [getUserInfos.pending]: (state) => {
+      state.status = "loading";
+    },
+    [getUserInfos.fulfilled]: (state, { payload }) => {
+      state.status = "succeded";
+      state.userDatas = payload;
+    },
+    [getUserInfos.rejected]: (state, { payload }) => {
+      state.status = "failed";
+      state.error = payload.message;
+    },
+  },
+});
+
+export const { setUserDatas, setIsAuth, setUserLogout } = userSlice.actions;
+export default userSlice.reducer;
