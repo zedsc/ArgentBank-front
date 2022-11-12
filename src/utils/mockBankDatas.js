d86bcf6a34168a6130fd49f2d419e8941d687784
ZSchmitt <zaiane.schmitt@gmail.com>
2022-11-12 21:04:02 +0100

    refactor: mocks datas

---
+++ src/utils/mockBankDatas.js
@@ -0,0 +1,20 @@
+export const bankDatas = [
+  {
+    title: "Argent Bank Checking",
+    lastNb: 8349,
+    balance: 2082.79,
+    balanceDesc: "Available Balance",
+  },
+  {
+    title: "Argent Banl Savings",
+    lastNb: 6712,
+    balance: 10928.42,
+    balanceDesc: "Available Balance",
+  },
+  {
+    title: "Argent Bank Credit Card",
+    lastNb: 8349,
+    balance: 184.3,
+    balanceDesc: "Current Balance",
+  },
+];
