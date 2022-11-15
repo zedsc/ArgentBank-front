c7e67dffbc91534556c9e1906733a2b2ae0ed0c2
ZSchmitt <zaiane.schmitt@gmail.com>
2022-11-15 18:56:29 +0100

    refactor: redux persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,56 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storageSession from "redux-persist/lib/storage/session";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import userReducer from "../feature/user.slice";
+import authReducer from "../feature/auth.slice";
 
-export default configureStore({
-  reducer: {},
+const rootPersistConfig = {
+  key: "root",
+  storage: storageSession,
+};
+
+// Special config to keep 'remember-me' option and user credential (username) in local storage
+const authPersistConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["userName", "rememberMe"],
+};
+
+const rootReducer = combineReducers({
+  auth: persistReducer(authPersistConfig, authReducer),
+  user: userReducer,
 });
+
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+export const persistor = persistStore(store);
+
+// export default configureStore({
+//   reducer: {
+//     user: userReducer,
+//     auth: authReducer,
+//   },
+//   devTools: process.env.NODE_ENV !== "production",
+//   //devTools: false
+// });
