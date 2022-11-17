2c7167062a4db0b3e5fad0c64fc2a69b45316fe9
ZSchmitt <zaiane.schmitt@gmail.com>
2022-11-17 16:34:32 +0100

    refactor: add guarded route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import GuardedRoute from "./components/GuardedRoute";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import UserProfil from "./pages/UserProfil";
@@ -9,7 +10,9 @@ const App = () => {
     <Routes>
       <Route path="/" exact element={<Home />} />
       <Route path="/sign-in" exact element={<SignIn />} />
-      <Route path="/user" element={<UserProfil />} />
+      <Route element={<GuardedRoute />}>
+        <Route path="/user" element={<UserProfil />} />
+      </Route>
       <Route path="*" element={<Home />} />
     </Routes>
   );
