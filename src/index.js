873d478fe1a40c195552dca902e7e0d8bb85647a
ZSchmitt <zaiane.schmitt@gmail.com>
2022-11-19 16:26:49 +0100

    feat: persist gate (redux persist)

--- src/index.js
+++ src/index.js
@@ -2,34 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./app/store";
+import { persistor, store } from "./app/store";
 import { BrowserRouter } from "react-router-dom";
-import { createGlobalStyle } from "styled-components";
-import { degradedGreys } from "./utils/colors";
-
-const GlobalStyle = createGlobalStyle`
-* {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: ${degradedGreys.gray2};
-}
-
-body {
-  margin: 0;
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-}
-`;
+import GlobalStyles from "./utils/GlobalStyles";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <GlobalStyle />
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <GlobalStyles />
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
