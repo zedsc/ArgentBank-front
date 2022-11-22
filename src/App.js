478cc51e67f811e2fd13611f41743f0a36781234
ZSchmitt <zaiane.schmitt@gmail.com>
2022-11-22 19:00:26 +0100

    feat: transactions page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import GuardedRoute from "./components/GuardedRoute";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
+import Transactions from "./pages/Transactions";
 import UserProfil from "./pages/UserProfil";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" exact element={<Home />} />
-      <Route path="/sign-in" exact element={<SignIn />} />
+      <Route path="/login" exact element={<SignIn />} />
       <Route element={<GuardedRoute />}>
-        <Route path="/user" element={<UserProfil />} />
+        <Route path="/profile" element={<UserProfil />} />
+        <Route path="/transactions" element={<Transactions />} />
       </Route>
       <Route path="*" element={<Home />} />
     </Routes>
---
+++ src/pages/Transactions.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/header/Header";
+import styled from "styled-components";
+import { gradientGreys, colors } from "../utils/colors";
+import TransactionsTable from "../components/transactionsTable/TransactionsTable";
+import { useLocation } from "react-router-dom";
+import { formatNumber } from "../utils/formatNumber";
+
+const DarkBg = styled.main`
+  flex: 1;
+  background-color: ${gradientGreys.gray3};
+`;
+
+const PageContent = styled.div`
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const TransactionsSection = styled.section`
+  text-align: center;
+`;
+
+const SrTitle = styled.h2`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  overflow: hidden;
+`;
+
+const AccountInfos = styled.header`
+  background-color: ${colors.white};
+  margin-top: 3px;
+  padding: 20px 0;
+  margin-bottom: 70px;
+`;
+
+const AccountName = styled.p`
+  margin: 0.5rem;
+`;
+
+const AccountBalance = styled.p`
+  font-size: 2.5rem;
+  font-weight: bold;
+  margin: 0.5rem;
+`;
+
+const AccountDesc = styled.p`
+  margin: 0.5rem;
+`;
+
+const Transactions = () => {
+  const location = useLocation();
+  const { userAccount } = location.state;
+  console.log(userAccount);
+  return (
+    <PageContent>
+      <Header />
+      <DarkBg>
+        <TransactionsSection>
+          <SrTitle>Transactions</SrTitle>
+          <AccountInfos>
+            <AccountName>
+              {userAccount.title} (x{userAccount.lastNb})
+            </AccountName>
+            <AccountBalance>{formatNumber(userAccount.balance)}</AccountBalance>
+            <AccountDesc>{userAccount.balanceDesc}</AccountDesc>
+          </AccountInfos>
+          <TransactionsTable
+            accountBalance={formatNumber(userAccount.balance)}
+          />
+        </TransactionsSection>
+      </DarkBg>
+      <Footer />
+    </PageContent>
+  );
+};
+
+export default Transactions;
